adcb1d1fc3638f45db57f2bcf027b2a8909e3632
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-07-29 09:31:10 +0800

    feat: enhance styling and structure by adding background component, updating CSS, and integrating new font utilities

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.css";
+
+const Background = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main} />
+      <div className={styles.triangle} />
+    </div>
+  );
+};
+
+export default Background;
---
+++ src/constants/fonts.ts
@@ -0,0 +1,16 @@
+import { Lexend, Montserrat, Oswald } from "next/font/google";
+
+export const MONTSERRAT = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const OSWALD = Oswald({
+  variable: "--font-oswald",
+  subsets: ["latin"],
+});
+
+export const LEXEND = Lexend({
+  variable: "--font-lexend",
+  subsets: ["latin"],
+});
