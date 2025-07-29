e05c44b4442fef3d6117eb85b845daefaab1efa5
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-07-29 17:26:29 +0800

    refactor: simplify GhostButton CSS by using nested selectors for better readability

--- src/components/Buttons/GhostButton/index.tsx
+++ src/components/Buttons/GhostButton/index.tsx
@@ -4,23 +4,6 @@ import React from "react";
 
 import styles from "./index.module.css";
 
-const hoverDirectionMap = {
-  up: "[&>*]:bottom-0 [&>*]:group-hover:bottom-1",
-  bottom: "[&>*]:top-0 [&>*]:group-hover:top-1",
-  left: "[&>*]:right-0 [&>*]:group-hover:right-1",
-  right: "[&>*]:left-0 [&>*]:group-hover:left-1",
-} as const;
-
-const hoverColorMap = {
-  accent: "[&>*]:group-hover:text-accent",
-} as const;
-
-const colorMap = {
-  primary: "[&>*]:text-foreground",
-  secondary: "[&>*]:text-foreground-2",
-  accent: "[&>*]:text-accent",
-} as const;
-
 type GhostButtonProps = React.ComponentProps<"button"> & {
   asChild?: boolean;
   hoverDirection?: "up" | "bottom" | "left" | "right";
