1333365a526682ed48efb9102c55e301b9b85ddd
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-08-02 11:57:42 +0800

    style: adjust padding in VerticalLines component and update line visibility for mobile responsiveness

--- src/components/BackgroundDecorations/VerticalLines/index.tsx
+++ src/components/BackgroundDecorations/VerticalLines/index.tsx
@@ -1,10 +1,14 @@
+import { cn } from "@/utils";
 import styles from "./index.module.css";
 
 const VerticalLines = () => {
   return (
     <div className={styles.verticalLines}>
       {Array.from({ length: 3 }).map((_, index) => (
-        <div key={index} className={styles.line} />
+        <div
+          key={index}
+          className={cn(styles.line, "max-sm:invisible max-sm:last:visible")}
+        />
       ))}
     </div>
   );
