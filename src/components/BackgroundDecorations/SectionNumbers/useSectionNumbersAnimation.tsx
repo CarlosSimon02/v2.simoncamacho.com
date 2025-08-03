e00ba9248686dd25127df590105af3eb3f5f9f0c
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-08-03 21:33:03 +0800

    feat: add Zustand for state management and enhance GSAP animations in SectionNumbers and Socials components with responsive breakpoints

--- src/components/BackgroundDecorations/SectionNumbers/useSectionNumbersAnimation.tsx
+++ src/components/BackgroundDecorations/SectionNumbers/useSectionNumbersAnimation.tsx
@@ -1,20 +1,42 @@
+import { CONDITIONAL_BREAKPOINTS } from "@/constants/breakpoints";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
 import { SECTION_NUMBER_CLASS } from "./constants";
 
-const useSectionNumbersAnimation = () => {
+type UseSectionNumbersAnimationProps = {
+  onComplete?: gsap.Callback;
+};
+
+const useSectionNumbersAnimation = ({
+  onComplete,
+}: UseSectionNumbersAnimationProps = {}) => {
   const sectionNumbersRef = useRef(null);
 
   useGSAP(
     () => {
-      gsap.to(`.${SECTION_NUMBER_CLASS}`, {
-        right: 0,
-        opacity: 1,
-        stagger: 0.1,
-        duration: 1,
-        ease: "power4.out",
-      });
+      const mm = gsap.matchMedia();
+
+      mm.add(
+        {
+          isXl: CONDITIONAL_BREAKPOINTS.isXl,
+          isMaxXl: CONDITIONAL_BREAKPOINTS.isMaxXl,
+        },
+        (context) => {
+          const { isXl } = context.conditions as gsap.Conditions;
+
+          if (!isXl) return;
+
+          gsap.to(`.${SECTION_NUMBER_CLASS}`, {
+            right: 0,
+            opacity: 1,
+            stagger: 0.1,
+            duration: 1,
+            ease: "power4.out",
+            onComplete,
+          });
+        }
+      );
     },
     { scope: sectionNumbersRef }
   );
--- src/components/BackgroundDecorations/Socials/useSocialsAnimation.tsx
+++ src/components/BackgroundDecorations/Socials/useSocialsAnimation.tsx
@@ -1,3 +1,4 @@
+import { CONDITIONAL_BREAKPOINTS } from "@/constants/breakpoints";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
@@ -8,13 +9,27 @@ const useSocialsAnimation = () => {
 
   useGSAP(
     () => {
-      gsap.to(`.${SOCIAL_CLASS}`, {
-        left: 0,
-        opacity: 1,
-        stagger: 0.1,
-        duration: 1,
-        ease: "power4.out",
-      });
+      const mm = gsap.matchMedia();
+
+      mm.add(
+        {
+          isXl: CONDITIONAL_BREAKPOINTS.isXl,
+          isMaxXl: CONDITIONAL_BREAKPOINTS.isMaxXl,
+        },
+        (context) => {
+          const { isXl } = context.conditions as gsap.Conditions;
+
+          if (!isXl) return;
+
+          gsap.to(`.${SOCIAL_CLASS}`, {
+            left: 0,
+            opacity: 1,
+            stagger: 0.1,
+            duration: 1,
+            ease: "power4.out",
+          });
+        }
+      );
     },
     { scope: socialsRef }
   );
