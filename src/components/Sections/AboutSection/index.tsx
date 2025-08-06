42e2e45bcf327bd94ac522d73b3152e5d14ece15
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-08-06 22:47:46 +0800

    feat: add AboutSectionAnimation for scroll-triggered animations and define styling constants for journey and skills sections

--- src/components/Sections/AboutSection/index.tsx
+++ src/components/Sections/AboutSection/index.tsx
@@ -3,6 +3,8 @@ import SectionTitle from "@/components/SectionTitle";
 import { TECHNOLOGIES } from "@/constants/technologies";
 import { cn } from "@/utils";
 import { useTranslations } from "next-intl";
+import AboutSectionAnimation from "./AboutSectionAnimation";
+import { MY_JOURNEY_STYLE, MY_SKILLS_STYLE } from "./constants";
 import Cubes from "./Cubes";
 import styles from "./index.module.css";
 
@@ -18,47 +20,56 @@ const AboutSection = ({ className }: AboutSectionProps) => {
   const t = useTranslations("aboutSection");
 
   return (
-    <ContentContainer
-      sectionId="about"
-      className={cn(className, styles.aboutSectionContainer)}
-    >
-      <SectionTitle title={t("title") as string} number={1} />
-      <div className={styles.aboutSectionMain}>
-        <div className={styles.aboutSectionTextCol}>
-          <SubHeader>{t("journeyHeader")}</SubHeader>
-          <p>
-            {t.rich("journey", {
-              br: () => <br />,
-            })}
-          </p>
+    <>
+      <AboutSectionAnimation />
+      <ContentContainer
+        sectionId="about"
+        className={cn(className, styles.aboutSectionContainer)}
+      >
+        <SectionTitle title={t("title") as string} number={1} />
+        <div className={styles.aboutSectionMain}>
+          <div className={cn(styles.aboutSectionTextCol, MY_JOURNEY_STYLE)}>
+            <SubHeader>{t("journeyHeader")}</SubHeader>
+            <p>
+              {t.rich("journey", {
+                br: () => <br />,
+              })}
+            </p>
+          </div>
+          <Cubes />
         </div>
-        <Cubes />
-      </div>
-      <div className={styles.aboutSectionSkills}>
-        <SubHeader>{t("skillsHeader")}</SubHeader>
-        <div className={styles.aboutSectionSkillsGrid}>
-          {TECHNOLOGIES.map((technology) => (
-            <div key={technology.name} className={styles.aboutSectionSkillCard}>
+        <div className={cn(styles.aboutSectionSkills, MY_SKILLS_STYLE)}>
+          <SubHeader>{t("skillsHeader")}</SubHeader>
+          <div className={styles.aboutSectionSkillsGrid}>
+            {TECHNOLOGIES.map((technology) => (
               <div
-                className={cn(
-                  styles.aboutSectionSkillIcon,
-                  technology.color === "system" && "bg-black/5 dark:bg-white/5"
-                )}
-                style={{
-                  backgroundColor:
-                    technology.color !== "system"
-                      ? technology.color + "20"
-                      : undefined,
-                }}
+                key={technology.name}
+                className={styles.aboutSectionSkillCard}
               >
-                <technology.icon className="size-6" />
+                <div
+                  className={cn(
+                    styles.aboutSectionSkillIcon,
+                    technology.color === "system" &&
+                      "bg-black/5 dark:bg-white/5"
+                  )}
+                  style={{
+                    backgroundColor:
+                      technology.color !== "system"
+                        ? technology.color + "20"
+                        : undefined,
+                  }}
+                >
+                  <technology.icon className="size-6" />
+                </div>
+                <p className={styles.aboutSectionSkillName}>
+                  {technology.name}
+                </p>
               </div>
-              <p className={styles.aboutSectionSkillName}>{technology.name}</p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </ContentContainer>
+      </ContentContainer>
+    </>
   );
 };
 
