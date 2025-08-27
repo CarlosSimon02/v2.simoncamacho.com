ed9a34cc37f10e2e0ebacd9447496b9723334a6f
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-08-27 18:03:26 +0800

    feat: add decorative animated cubes to NotFound page for enhanced visual appeal

--- src/app/[locale]/(main)/not-found.tsx
+++ src/app/[locale]/(main)/not-found.tsx
@@ -1,14 +1,34 @@
+"use client";
+
 import PillButton from "@/components/UI/Buttons/PillButton";
 import BaseContainer from "@/components/UI/Containers/BaseContainer";
+import Cube from "@/components/UI/Effects/Cube";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center gap-4 px-12 text-center">
+    <main className="flex flex-1 flex-col items-center justify-center gap-4 px-3 text-center md:px-12">
       <BaseContainer className="relative">
+        {/* Decorative animated cubes */}
+        <div className="pointer-events-none absolute top-0 right-60 -z-2 max-md:hidden">
+          <Cube
+            cubeSize={110}
+            defaultAngle={{ x: 80, y: 10 }}
+            autoRotate
+            className="opacity-70"
+          />
+        </div>
+        <div className="pointer-events-none absolute -bottom-20 left-50 -z-2 mt-14 ml-8 max-md:hidden">
+          <Cube
+            cubeSize={80}
+            defaultAngle={{ x: 10, y: 180 }}
+            autoRotate
+            className="opacity-70"
+          />
+        </div>
         <div>
-          <div className="relative -z-10">
-            <h1 className="text-decor text-[12rem] leading-tight select-none md:text-[16rem]">
+          <div className="relative -z-10 flex flex-col items-center justify-center gap-4 text-center">
+            <h1 className="text-decor text-center text-[12rem] leading-tight select-none md:text-[16rem]">
               404
             </h1>
           </div>
