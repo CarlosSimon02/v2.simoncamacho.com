ed1d816f6e69f15ffe3b9cc7c2ad90fbaac99173
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-08-31 10:28:26 +0800

    feat: implement ConversationInterface component in Chat, enhancing user interaction by displaying query and chat input; refactor Chat component for improved structure

--- src/components/Features/Chat/Chat.tsx
+++ src/components/Features/Chat/Chat.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import jackImage from "@/assets/heroes/jack.png";
-import ChatContentContainer from "@/components/UI/Containers/ChatContentContainer";
-import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import ChatInput from "./components/ChatInput";
+import ConversationInterface from "./components/ConversationInterface";
 import EmptyQueryState from "./components/EmptyQueryState";
 
 const Chat = () => {
@@ -15,41 +12,7 @@ const Chat = () => {
     return <EmptyQueryState />;
   }
 
-  return (
-    <>
-      <ChatContentContainer className="flex self-stretch">
-        <div className="h-[1024px] w-full py-10">
-          <div className="flex w-full justify-end pb-10">
-            <p className="bg-bg-user-chat text-fg-secondary ml-10 max-w-2xl rounded-3xl rounded-br-none px-5 py-3 text-sm">
-              Hello, how are you doing today?
-            </p>
-          </div>
-          <div className="flex items-start gap-3 !text-sm md:gap-6">
-            <Image
-              height={100}
-              width={100}
-              src={jackImage.src}
-              alt="Jack"
-              className="size-9 md:size-12"
-            />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </div>
-      </ChatContentContainer>
-      <ChatContentContainer className="fixed bottom-0 left-1/2 flex w-full -translate-x-1/2 flex-col items-center gap-1 py-3">
-        <ChatInput />
-        <p className="text-[0.75rem]">AI-generated, for reference only</p>
-      </ChatContentContainer>
-    </>
-  );
+  return <ConversationInterface query={query} />;
 };
 
 export default Chat;
--- src/components/Features/Chat/components/ConversationInterface.tsx
+++ src/components/Features/Chat/components/ConversationInterface.tsx
@@ -1,5 +1,48 @@
-const ConversationInterface = () => {
-  return <div>ConversationInterface</div>;
+import jackImage from "@/assets/heroes/jack.png";
+import ChatContentContainer from "@/components/UI/Containers/ChatContentContainer";
+import Image from "next/image";
+import ChatInput from "./ChatInput";
+
+type ConversationInterfaceProps = {
+  query: string;
+};
+
+const ConversationInterface = ({ query }: ConversationInterfaceProps) => {
+  return (
+    <>
+      <ChatContentContainer className="flex self-stretch">
+        <div className="h-[1024px] w-full py-10">
+          <div className="flex w-full justify-end pb-10">
+            <p className="bg-bg-user-chat text-fg-secondary ml-10 max-w-2xl rounded-3xl rounded-br-none px-5 py-3 text-sm">
+              {query}
+            </p>
+          </div>
+          <div className="flex items-start gap-3 !text-sm md:gap-6">
+            <Image
+              height={100}
+              width={100}
+              src={jackImage.src}
+              alt="Jack"
+              className="size-9 md:size-12"
+            />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </ChatContentContainer>
+      <ChatContentContainer className="fixed bottom-0 left-1/2 flex w-full -translate-x-1/2 flex-col items-center gap-1 py-3">
+        <ChatInput />
+        <p className="text-[0.75rem]">AI-generated, for reference only</p>
+      </ChatContentContainer>
+    </>
+  );
 };
 
 export default ConversationInterface;
