5f6048d80e632465a8786704b7941c66f77b0a16
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-08-31 16:14:03 +0800

    feat: update chat API route to utilize MockLanguageModelV2 for simulated streaming responses; modify POST method to return UI message stream response

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,11 +1,31 @@
-import { openai } from "@ai-sdk/openai";
-import { generateText } from "ai";
+import { simulateReadableStream, streamText } from "ai";
+import { MockLanguageModelV2 } from "ai/test";
 
-export async function POST() {
-  const { text } = await generateText({
-    model: openai("gpt-5-nano"),
-    prompt: "Write a short poem about the sea.",
+export async function POST(req: Request) {
+  const result = streamText({
+    // model: openai("gpt-5-nano"),
+    model: new MockLanguageModelV2({
+      doStream: async () => ({
+        stream: simulateReadableStream({
+          chunkDelayInMs: 1000,
+          chunks: [
+            { type: "text-start", id: `text-1` },
+            { type: "text-delta", id: "text-1", delta: "Hello" },
+            { type: "text-delta", id: "text-1", delta: ", " },
+            { type: "text-delta", id: "text-1", delta: "world!" },
+            { type: "text-end", id: "text-1" },
+            {
+              type: "finish",
+              finishReason: "stop",
+              logprobs: undefined,
+              usage: { inputTokens: 3, outputTokens: 10, totalTokens: 13 },
+            },
+          ],
+        }),
+      }),
+    }),
+    prompt: "Hello, world!",
   });
 
-  return Response.json({ text });
+  return result.toUIMessageStreamResponse();
 }
