34e033f73479a1b4733e3538024d286966e30abd
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-09-01 08:19:56 +0800

    fix: update font constants to include preload and display properties for improved performance and styling

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -3,14 +3,17 @@ import { Lexend, Montserrat, Oswald } from "next/font/google";
 export const MONTSERRAT = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
+  preload: false,
 });
 
 export const OSWALD = Oswald({
   variable: "--font-oswald",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const LEXEND = Lexend({
   variable: "--font-lexend",
   subsets: ["latin"],
+  preload: false,
 });
