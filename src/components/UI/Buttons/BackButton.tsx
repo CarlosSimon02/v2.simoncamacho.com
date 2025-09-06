b742f90deb8934b9ce22a59ebb175a53f205a876
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-09-06 17:01:11 +0800

    feat: implement BackButton component with navigation functionality, allowing users to return to the previous page or home based on history state

--- src/components/UI/Buttons/BackButton.tsx
+++ src/components/UI/Buttons/BackButton.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/utils";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 import GhostButton from "./GhostButton";
 
 type BackButtonProps = {
@@ -11,12 +12,22 @@ type BackButtonProps = {
 
 const BackButton = ({ className }: BackButtonProps) => {
   const t = useTranslations();
+  const router = useRouter();
+
+  const handleBack = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  };
 
   return (
     <GhostButton
       color="secondary"
       className={cn("font-oswald flex items-center gap-2", className)}
       aria-label={t("common.backButtonLabel")}
+      onClick={handleBack}
     >
       <ArrowLeftIcon className="size-6" />
     </GhostButton>
