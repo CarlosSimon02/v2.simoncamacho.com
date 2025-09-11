506c8fa3283ef52b86e23bcaa60371f67ff2a14f
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-09-11 16:48:19 +0800

    feat: add TechnologyCard component for better technology representation and update ProjectOverviewSection to display technologies with improved layout

--- src/components/Features/Sections/ProjectOverviewSection/ProjectOverviewSection.tsx
+++ src/components/Features/Sections/ProjectOverviewSection/ProjectOverviewSection.tsx
@@ -2,7 +2,9 @@ import pomodoro from "@/assets/projects/pomodoro.png";
 import pomodoroLogo from "@/assets/projects/pomodoroLogo.png";
 import PillButton from "@/components/UI/Buttons/PillButton";
 import ContentContainer from "@/components/UI/Containers/ContentContainer";
+import { CssIcon, HtmlIcon, SassIcon } from "@/components/UI/Icons/Techs";
 import ImageWithGridBg from "@/components/UI/ImageWithGridBg";
+import TechnologyCard from "@/components/UI/TechnologyCard";
 import { cn } from "@/utils";
 
 type ProjectOverviewSectionProps = {
@@ -10,6 +12,24 @@ type ProjectOverviewSectionProps = {
   projectName: string;
   description: string;
 };
+
+const sampleTechnologies = [
+  {
+    name: "HTML",
+    icon: HtmlIcon,
+    color: "#E34F26",
+  },
+  {
+    name: "CSS",
+    icon: CssIcon,
+    color: "#1572B6",
+  },
+  {
+    name: "Sass",
+    icon: SassIcon,
+    color: "#CC6699",
+  },
+];
 
 const ProjectOverviewSection = ({
   className,
@@ -19,7 +39,7 @@ const ProjectOverviewSection = ({
   return (
     <ContentContainer
       sectionId="project-overview"
-      className={cn("flex flex-col gap-9 md:gap-12", className)}
+      className={cn("flex flex-col gap-18 md:gap-18", className)}
     >
       <h1 className="font-montserrat text-accent flex-1 text-3xl leading-none font-black md:text-4xl lg:text-5xl">
         {projectName}
@@ -59,15 +79,39 @@ const ProjectOverviewSection = ({
         </div>
       </div>
       <div className="flex items-center gap-8 max-md:flex-col md:gap-12">
-        <div className="flex flex-1 flex-col gap-4 md:gap-5">
+        <div className="flex flex-col gap-4 md:flex-[60%] md:gap-5">
           <h2 className="subheading">Web Stack and Technologies</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit.
-          </p>
+          <div className="flex flex-col gap-4">
+            <div className="flex w-full flex-wrap items-stretch justify-center gap-3 md:hidden">
+              {sampleTechnologies.map((technology) => (
+                <TechnologyCard
+                  key={technology.name}
+                  technology={technology}
+                  className="w-full max-w-[12rem]"
+                />
+              ))}
+            </div>
+            <p className="flex-2/3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit.
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full flex-[40%] flex-wrap items-stretch justify-center gap-3 max-md:hidden">
+          {sampleTechnologies.map((technology) => (
+            <TechnologyCard
+              key={technology.name}
+              technology={technology}
+              className="w-full max-w-[12rem]"
+            />
+          ))}
         </div>
       </div>
     </ContentContainer>
--- src/components/UI/SkillsGrid.tsx
+++ src/components/UI/SkillsGrid.tsx
@@ -1,6 +1,7 @@
 import { TECHNOLOGIES } from "@/data/technologies";
 import { cn } from "@/utils";
 import { Skeleton } from "../Primitives/Skeleton";
+import TechnologyCard from "./TechnologyCard";
 
 type SkillsGridLoadingProps = {
   className?: string;
@@ -67,45 +68,11 @@ const SkillsGrid = ({ className, variant = "default" }: SkillsGridProps) => {
       )}
     >
       {TECHNOLOGIES.map((technology) => (
-        <div
+        <TechnologyCard
           key={technology.name}
-          className={cn(
-            "bg-bg-card flex items-center gap-4 rounded-xl",
-            variant === "default" && " p-2",
-            variant === "small" && "gap-2 rounded-lg p-1"
-          )}
-        >
-          <div
-            className={cn(
-              "flex items-center justify-center rounded-lg",
-              variant === "default" && "p-2 text-black dark:text-white",
-              variant === "small" &&
-                "p-1 text-xs text-black/80 dark:text-white/80",
-              technology.color === "system" && "bg-black/5 dark:bg-white/5"
-            )}
-            style={{
-              backgroundColor:
-                technology.color !== "system"
-                  ? technology.color + (variant === "default" ? "30" : "20")
-                  : undefined,
-            }}
-          >
-            <technology.icon
-              className={cn(
-                variant === "default" && "size-6",
-                variant === "small" && "size-4"
-              )}
-            />
-          </div>
-          <p
-            className={cn(
-              "text-medium-gray-50 dark:text-light-gray-950",
-              variant === "small" && "text-sm"
-            )}
-          >
-            {technology.name}
-          </p>
-        </div>
+          technology={technology}
+          variant={variant}
+        />
       ))}
     </div>
   );
