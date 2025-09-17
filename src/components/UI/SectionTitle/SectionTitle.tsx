a047f729a5356cd4697e653baf5b3d54acb6c283
Carlos Simon Camacho <caloycamacho02@gmail.com>
2025-09-17 20:52:34 +0800

    refactor: enhance SectionTitle component by integrating GSAP animations and removing unused utility functions for improved performance and maintainability

--- src/components/UI/SectionTitle/SectionTitle.tsx
+++ src/components/UI/SectionTitle/SectionTitle.tsx
@@ -1,38 +1,70 @@
+"use client";
+
 import { cn, toKebabCase } from "@/utils";
-import SectionTitleAnimation from "./components/SectionTitleAnimation";
-import { getSectionTitleContainerStyle } from "./utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 type SectionTitleProps = {
   title: string;
   number: number;
 };
 
+const SECTION_TITLE_ITEM_CLASS = "section-title-item";
+const SECTION_TITLE_CONTAINER_CLASS = "section-title-container";
+
 const SectionTitle = ({ title, number }: SectionTitleProps) => {
   const formattedTitle = toKebabCase(title);
-  const containerStyle = getSectionTitleContainerStyle(formattedTitle);
+  const containerClass = `${SECTION_TITLE_CONTAINER_CLASS}-${formattedTitle}`;
+  const itemClass = `${SECTION_TITLE_ITEM_CLASS}-${formattedTitle}`;
+
+  useGSAP(() => {
+    gsap.to(`.${itemClass}`, {
+      left: 0,
+      opacity: 1,
+      duration: 2,
+      ease: "power4.out",
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: `.${containerClass}`,
+        start: "top 70%",
+      },
+    });
+  });
 
   return (
-    <>
-      <SectionTitleAnimation id={formattedTitle} />
+    <div
+      className={cn(
+        "flex items-center justify-between gap-16 md:gap-24 lg:gap-56",
+        containerClass
+      )}
+    >
+      <div className="flex flex-col gap-3">
+        <span
+          className={cn(
+            "font-oswald text-light-gray-300 dark:text-dark-gray-700 from-right-sm text-5xl font-black md:text-6xl",
+            itemClass
+          )}
+        >
+          {String(number).padStart(2, "0")}
+        </span>
+        <h2
+          className={cn(
+            "font-montserrat text-accent from-right-sm w-[8.75rem] text-3xl font-black whitespace-nowrap md:text-4xl",
+            itemClass
+          )}
+        >
+          {title}
+        </h2>
+      </div>
       <div
         className={cn(
-          "flex items-center justify-between gap-16 md:gap-24 lg:gap-56",
-          containerStyle
+          "text-decor from-right-sm text-9xl select-none md:text-[10rem]",
+          itemClass
         )}
       >
-        <div className="flex flex-col gap-3">
-          <span className="font-oswald text-light-gray-300 dark:text-dark-gray-700 text-5xl font-black md:text-6xl">
-            {String(number).padStart(2, "0")}
-          </span>
-          <h2 className="font-montserrat text-accent w-[8.75rem] text-3xl font-black whitespace-nowrap md:text-4xl">
-            {title}
-          </h2>
-        </div>
-        <div className="text-decor text-9xl select-none md:text-[10rem]">
-          {title.slice(0, 10)}
-        </div>
+        {title.slice(0, 10)}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/UI/SectionTitle/utils.ts
+++
@@ -1,5 +0,0 @@
-import { SECTION_TITLE_CONTAINER_CLASS } from "./constants";
-
-export const getSectionTitleContainerStyle = (title: string) => {
-  return `${SECTION_TITLE_CONTAINER_CLASS}-${title} from-bottom-xs`;
-};
